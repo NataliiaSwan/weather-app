557f4049c448b88856212ae30374d37b70e9dafb
NataliiaSwan <shapnat69@gmail.com>
2025-08-06 21:50:29 +0300

    Add icons

--- src/index.js
+++ src/index.js
@@ -25,7 +25,8 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-  console.log(response.data);
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
 
   cityElement.innerHTML = cityName;
 
